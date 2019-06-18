da9895206b51faaa7580655be074652889e2ca19
juaxE <juho.kirvesmaki@helsinki.fi>
2019-06-18 13:37:14 +0300

    1.5

--- osa1/kurssit/src/index.js
+++ osa1/kurssit/src/index.js
@@ -2,28 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
             name: 'Fundamentals of React',
             exercises: 10
-        },
-        {
+          },
+          {
             name: 'Using props to pass data',
             exercises: 7
-        },
-        {
+          },
+          {
             name: 'State of a component',
             exercises: 14
-        }
-    ]
+          }
+        ]
+      }
 
 
     return (
         <div>
-            <Header name={course} />
-            <Content parts = {parts} />
-            <Total parts = {parts} />
+            <Header name={course.name} />
+            <Content parts = {course.parts} />
+            <Total parts = {course.parts} />
         </div>
     )
 }
