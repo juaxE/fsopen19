ad98f3dd0357706925fa74d1f7f652c590476c0e
jurbelo <juho.kirvesmaki@selko.io>
2019-10-22 14:29:07 +0300

    2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newCondition, setNewCondition] = useState('')
   
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -17,6 +21,10 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewCondition(event.target.value)
+  }
+
   const newPerson = (event) => {
     event.preventDefault()
     const personObject = {
@@ -24,7 +32,7 @@ const App = () => {
       number: newNumber
     }
     const names = persons.map(person => person.name)
-    names.includes(newName) ?
+    names.includes(newName.toUpper) ?
       window.alert(`${newName} is already in the phonebook`)
       : setPersons(persons.concat(personObject))
     setNewName('')
@@ -35,12 +43,18 @@ const App = () => {
   
   
   const numbers = () =>
-    persons.map(person =>
+    persons.filter(person => person.name.toUpperCase().includes(newCondition.toUpperCase())).map(person =>
       <li key={person.name}>{person.name} {person.number}</li>)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newCondition}
+            onChange={handleFilterChange}
+          />
+      </div>
+      <h3>add a new number</h3>
       <form onSubmit={newPerson}>
         <div>
           name: <input value={newName}
